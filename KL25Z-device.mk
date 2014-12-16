e870ba68b6980e20f441e4f2ca217088abb9e9e5
Richard Osterloh <richard.osterloh@gmail.com>
2014-12-16 10:34:44 +0000

    Cleanup from upstream with new build macros

--- KL25Z-device.mk
+++ KL25Z-device.mk
@@ -5,15 +5,9 @@ MBED_DEVICE        := KL25Z
 MBED_TARGET        := Freescale_KL25Z
 MBED_CLEAN         := $(MBED_DEVICE)-MBED-clean
 
-# Some libraries (mbed and rtos) have device specific source folders.
-HAL_TARGET_SRC   := $(TARGETS_HAL)/TARGET_Freescale/TARGET_KLXX
-HAL_TARGET_SRC   += $(TARGETS_HAL)/TARGET_Freescale/TARGET_KLXX/TARGET_KL25Z
-CMSIS_TARGET_SRC := $(CMSIS_COMMON_HEADERS)/TARGET_Freescale/TARGET_KLXX/TARGET_KL25Z
-CMSIS_TARGET_SRC += $(CMSIS_COMMON_HEADERS)/TARGET_Freescale/TARGET_KLXX/TARGET_KL25Z/TOOLCHAIN_GCC_ARM
-RTX_TARGET_SRC   := $(MBED_LIB_SRC_ROOT)/rtos/rtx/TARGET_CORTEX_M/TARGET_M0P/TOOLCHAIN_GCC
-
 # Compiler flags which are specifc to this device.
-GCC_DEFINES := -DTARGET_KL25Z -DTARGET_M0P -DTARGET_Freescale -DTARGET_KLXX
+TARGETS_FOR_DEVICE := TARGET_KL25Z TARGET_M0P TARGET_Freescale TARGET_KLXX TARGET_CORTEX_M
+GCC_DEFINES := $(patsubst %,-D%,$(TARGETS_FOR_DEVICE))
 GCC_DEFINES += -D__CORTEX_M0PLUS -DARM_MATH_CM0PLUS
 
 C_FLAGS   := -mcpu=cortex-m0plus -mthumb -mthumb-interwork
@@ -24,7 +18,7 @@ LD_FLAGS  := -mcpu=cortex-m0plus -mthumb
 DEVICE_OBJECTS :=
 
 # Linker script to be used.  Indicates what code should be placed where in memory.
-LSCRIPT=$(CMSIS_COMMON_HEADERS)/TARGET_Freescale/TARGET_KLXX/TARGET_KL25Z/TOOLCHAIN_GCC_ARM/MKL25Z4.ld
+LSCRIPT=$(MBED_CMSIS_ROOT)/TARGET_Freescale/TARGET_KLXX/TARGET_KL25Z/TOOLCHAIN_GCC_ARM/MKL25Z4.ld
 
 
 include $(WORKSPACE_ROOT)/build/device-common.mk
--- device-common.mk
+++ device-common.mk
@@ -14,18 +14,20 @@ RELEASE_OPTIMIZATION := s
 DEP_FLAGS := -MMD -MP
 
 # Preprocessor defines to use when compiling/assembling code with GCC.
-GCC_DEFINES += -DTOOLCHAIN_GCC_ARM -DTOOLCHAIN_GCC -D__MBED__=1
+GCC_DEFINES += $(TOOLCHAIN_DEFINES) -DTOOLCHAIN_GCC -D__MBED__=1
 
 # Flags to be used with C/C++ compiler that are shared between Debug and Release builds.
-#C_FLAGS += -g3 -ffunction-sections -fdata-sections -fno-exceptions -fno-delete-null-pointer-checks -fomit-frame-pointer
-#C_FLAGS += -Wall -Wextra -Wno-unused-parameter -Wno-missing-field-initializers -Wno-missing-braces
-C_FLAGS += -c -g -fno-common -fmessage-length=0 -Wall -fno-exceptions -ffunction-sections -fdata-sections -fomit-frame-pointer
+C_FLAGS += -g3 -ffunction-sections -fdata-sections -fno-exceptions -fno-delete-null-pointer-checks -fomit-frame-pointer
+C_FLAGS += -Wall -Wextra -Wno-unused-parameter -Wno-missing-field-initializers -Wno-missing-braces -fno-builtin -fmessage-length=0
+#C_FLAGS += -Wredundant-decls -Wundef -Wshadow
+#C_FLAGS += -c -g -fno-common
 C_FLAGS += $(GCC_DEFINES)
 C_FLAGS += $(DEP_FLAGS)
 
 # MBED uses -std=gnu++98 not -std=gnu++11
-CPP_FLAGS := $(C_FLAGS) -std=gnu++98 -fno-rtti
+CPP_FLAGS := $(C_FLAGS) -fno-rtti -std=gnu++11
 C_FLAGS   += -std=gnu99
+# C only but enabled by default -Wimplicit-function-declaration -Wmissing-prototypes -Wstrict-prototypes
 
 # Flags used to assemble assembly languages sources.
 ASM_FLAGS += -g3 -x assembler-with-cpp $(GCC_DEFINES)
@@ -47,23 +49,26 @@ OUTDIR := build/$(MBED_DEVICE)
 TARGET_BIN := $(OUTDIR)/$(PROJECT).bin
 
 # List of the objects files to be compiled/assembled based on source files in SRC.
-OBJECTS := $(call srcs2objs,$(call recurse_dir,$(SRC)),$(SRC),$(OUTDIR))
+OBJECTS := $(call srcs2objs,$(call filter_dirs,$(call recurse_dir,$(SRC)),$(TARGETS_FOR_DEVICE)),$(SRC),$(OUTDIR))
+
+# Add in device specific object file(s).
+OBJECTS += $(DEVICE_OBJECTS)
 
 # Initialize list of the header dependency files, one per object file. Each mbed SDK library will append to this list.
 DEPFILES := $(patsubst %.o,%.d,$(OBJECTS))
 
 # Include path which points to subdirectories of this project and user specified directory.
-INCLUDE_DIRS := $(patsubst %,-I%,$(INCDIRS) $(SRC) $(call recurse_dir,$(SRC)))
+INCLUDE_DIRS := $(patsubst %,-I%,$(INCDIRS) $(SRC) $(call filter_dirs,$(call recurse_dir,$(SRC)),$(TARGETS_FOR_DEVICE)))
 
 # DEFINEs to be used when building C/C++ code
 MAIN_DEFINES := $(DEFINES)
 
 # Libraries to be linked into final binary
-SYS_LIBS  := -lstdc++ -lsupc++ -lm -lc -lgcc -lnosys
+SYS_LIBS  := -lstdc++ -lsupc++ -lm -lgcc -lc -lgcc -lc -lnosys
 LIBS      := $(LIBS_PREFIX)
 
 # Some choices like mbed SDK library locations and enabling of asserts depend on build type.
-ifeq "$(GCC4MBED_TYPE)" "Debug"
+ifeq "$(BUILD_TYPE)" "Debug"
 MBED_LIBRARIES := $(patsubst %,$(DEBUG_DIR)/%.a,$(MBED_LIBS))
 else
 MBED_LIBRARIES := $(patsubst %,$(RELEASE_DIR)/%.a,$(MBED_LIBS))
@@ -79,11 +84,15 @@ $(MBED_DEVICE): CPP_FLAGS := -O$(OPTIMIZATION) $(CPP_FLAGS) $(MAIN_DEFINES) $(IN
 $(MBED_DEVICE): ASM_FLAGS := $(ASM_FLAGS) $(GAFLAGS) $(INCLUDE_DIRS)
 
 # Linker Options.
-#$(MBED_DEVICE): LD_FLAGS := $(LD_FLAGS) -Wl,--gc-sections -Wl,--wrap=main --specs=nano.specs
-$(MBED_DEVICE): LD_FLAGS := $(LD_FLAGS) -Wl,--gc-sections --specs=nano.specs
-#$(MBED_DEVICE): LD_FLAGS +=	-u _printf_float -u _scanf_float
-$(MBED_DEVICE): LD_FLAGS += -Wl,-Map=$(OUTDIR)/$(PROJECT).map,--cref
-#	-mfloat-abi=soft
+$(MBED_DEVICE): LD_FLAGS := $(LD_FLAGS) --specs=nano.specs -u mbed_sdk_init -u mbed_main
+$(MBED_DEVICE): LD_FLAGS += -Wl,-Map=$(OUTDIR)/$(PROJECT).map,--cref,--gc-sections,--wrap=main
+ifneq "$(NO_FLOAT_SCANF)" "1"
+$(MBED_DEVICE): LD_FLAGS += -u _scanf_float
+endif
+ifneq "$(NO_FLOAT_PRINTF)" "1"
+$(MBED_DEVICE): LD_FLAGS += -u _printf_float
+endif
+#	--wrap=_isatty,--wrap=malloc,--wrap=realloc,--wrap=free,--wrap=_read,--wrap=_write,--wrap=semihost_connected
 
 .PHONY: $(MBED_DEVICE) $(MBED_DEVICE)-clean $(MBED_DEVICE)-deploy $(MBED_DEVICE)-size
 
@@ -142,52 +151,74 @@ $(OUTDIR)/%.o : $(SRC)/%.S makefile
 ###############################################################################
 # Library mbed.a
 ###############################################################################
+MBED_DIRS := $(call filter_dirs,$(call recurse_dir,$(MBED_SRC_ROOT)),$(TARGETS_FOR_DEVICE))
 $(eval $(call build_lib,mbed,\
-                       $(HAL_TARGET_SRC) $(CMSIS_TARGET_SRC) $(COMMON_SRC),\
-                       $(API_HEADERS) $(HAL_HEADERS) $(CMSIS_COMMON_HEADERS) $(HAL_TARGET_SRC) $(CMSIS_TARGET_SRC)))
+												$(MBED_DIRS),\
+												$(MBED_DIRS)))
 
 ###############################################################################
 # Library rtos.a
 ###############################################################################
-$(eval $(call build_lib,rtos,\
-                       $(RTOS_DIRS) $(RTX_TARGET_SRC),\
-                       $(RTOS_DIRS)))
+ifeq "$(findstring rtos,$(MBED_LIBS))" "rtos"
+	RTOS_DIRS := $(call filter_dirs,$(call recurse_dir,$(MBED_LIB_SRC_ROOT)/rtos),$(TARGETS_FOR_DEVICE))
+	$(eval $(call build_lib,rtos,$(RTOS_DIRS),$(RTOS_DIRS)))
+endif
 
 ###############################################################################
 # Library net/lwip.a
 ###############################################################################
-$(eval $(call build_lib,net/lwip,$(LWIP_DIRS),$(LWIP_DIRS)))
+ifeq "$(findstring net/lwip,$(MBED_LIBS))" "net/lwip"
+	LWIP_DIRS := $(call filter_dirs,$(call recurse_dir,$(MBED_LIB_SRC_ROOT)/net/lwip),$(TARGETS_FOR_DEVICE))
+	$(eval $(call build_lib,net/lwip,$(LWIP_DIRS),$(LWIP_DIRS)))
+endif
 
 ###############################################################################
 # Library net/eth.a
 ###############################################################################
-$(eval $(call build_lib,net/eth,$(ETH_DIRS) $(ETH_TARGET_SRC),$(ETH_DIRS) $(ETH_TARGET_SRC)))
+ifeq "$(findstring net/eth,$(MBED_LIBS))" "net/eth"
+	ETH_DIRS := $(call filter_dirs,$(call recurse_dir,$(MBED_LIB_SRC_ROOT)/net/eth),$(TARGETS_FOR_DEVICE))
+	$(eval $(call build_lib,net/eth,$(ETH_DIRS),$(ETH_DIRS)))
+endif
 
 ###############################################################################
 # Library fs.a
 ###############################################################################
+ifeq "$(findstring fs,$(MBED_LIBS))" "fs"
+FS_DIRS := $(call filter_dirs,$(call recurse_dir,$(MBED_LIB_SRC_ROOT)/fs),$(TARGETS_FOR_DEVICE))
 $(eval $(call build_lib,fs,$(FS_DIRS),$(FS_DIRS)))
+endif
 
 ###############################################################################
 # Library USBDevice.a
 ###############################################################################
+ifeq "$(findstring USBDevice,$(MBED_LIBS))" "USBDevice"
+USB_DEVICE_DIRS := $(call filter_dirs,$(call recurse_dir,$(MBED_LIB_SRC_ROOT)/USBDevice),$(TARGETS_FOR_DEVICE))
 $(eval $(call build_lib,USBDevice,$(USB_DEVICE_DIRS),$(USB_DEVICE_DIRS)))
+endif
 
 ###############################################################################
 # Library USBHost.a
 ###############################################################################
+ifeq "$(findstring USBHost,$(MBED_LIBS))" "USBHost"
+USB_HOST_DIRS := $(call filter_dirs,$(call recurse_dir,$(MBED_LIB_SRC_ROOT)/USBHost),$(TARGETS_FOR_DEVICE))
 $(eval $(call build_lib,USBHost,$(USB_HOST_DIRS),$(USB_HOST_DIRS)))
+endif
 
 ###############################################################################
 # Library rpc.a
 ###############################################################################
+ifeq "$(findstring rpc,$(MBED_LIBS))" "rpc"
+RPC_DIRS := $(call filter_dirs,$(call recurse_dir,$(MBED_LIB_SRC_ROOT)/rpc),$(TARGETS_FOR_DEVICE))
 $(eval $(call build_lib,rpc,$(RPC_DIRS),$(RPC_DIRS)))
+endif
 
 ###############################################################################
 # Library dsp.a
 ###############################################################################
+ifeq "$(findstring dsp,$(MBED_LIBS))" "dsp"
+DSP_DIRS := $(call filter_dirs,$(call recurse_dir,$(MBED_LIB_SRC_ROOT)/dsp),$(TARGETS_FOR_DEVICE))
 $(eval $(call build_lib,dsp,$(DSP_DIRS),$(DSP_DIRS)))
-
+endif
 
 #########################################################################
 #  Default rules to compile c/c++/assembly language sources to objects.
